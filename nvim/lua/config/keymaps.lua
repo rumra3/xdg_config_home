222196a317f4f52ea62eac34363ab60ce38cfa05
ramura <44088736+rumra3@users.noreply.github.com>
2024-01-04 23:33:02 +0100

    remove zk and neoterm

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -61,48 +61,6 @@ vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>", {}) -- open/close
 vim.keymap.set("n", "<leader>nr", ":NvimTreeRefresh<CR>", {}) -- refresh
 vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>", {}) -- search file
 
--- neoterm
-vim.keymap.set("n", "<leader>tt", ":NeotermToggle<CR>", {})
-vim.keymap.set("n", "<C-x>", ":NeotermExit<CR>", {})
-
--- zk
-local opts = { noremap=true, silent=false }
--- new note
-vim.api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
--- Open notes.
-vim.api.nvim_set_keymap("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
--- Search for the notes matching a given query.
-vim.api.nvim_set_keymap("n", "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", opts)
--- Add the key mappings only for Markdown files in a zk notebook.
-if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
-	local function map(...) vim.api.nvim_buf_set_keymap(0, ...) end
-	local opts = { noremap=true, silent=false }
-
-	-- Open the link under the caret.
-	map("n", "<leader>zo", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-
-	-- Create a new note after asking for its title.
-	-- This overrides the global `<leader>zn` mapping to create the note in the same directory as the current buffer.
-	map("n", "<leader>zn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", opts)
-	-- Create a new note in the same directory as the current buffer, using the current selection for title.
-	map("v", "<leader>ztn", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", opts)
-	map("v", "<leader>ztb", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h'), edit = false }<CR>", opts)
-	-- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
-	map("v", "<leader>zcn", ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", opts)
-
-	-- Open notes linking to the current buffer.
-	map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", opts)
-	-- Alternative for backlinks using pure LSP and showing the source context.
-	--map('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-	-- Open notes linked by the current buffer.
-	map("n", "<leader>zl", "<Cmd>ZkLinks<CR>", opts)
-
-	-- Preview a linked note.
-	map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	-- Open the code actions for a visual selection.
-	map("v", "<leader>za", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", opts)
-end
-
 -- spider
 vim.keymap.set({"n", "o", "x"}, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
 vim.keymap.set({"n", "o", "x"}, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
