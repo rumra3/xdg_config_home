50ed992966bddae5c75bc967da1c14cdfe1486b9
ramura <44088736+rumra3@users.noreply.github.com>
2024-01-04 23:33:02 +0100

    unbind tab, stab, cr

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -25,31 +25,10 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-z>'] = cmp.mapping.confirm({ select = true }),
 	["<C-Space>"] = cmp.mapping.complete(),
-	["<Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_next_item()
-			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-			-- they way you will only jump inside the snippet region
-		elseif luasnip.expand_or_jumpable() then
-			luasnip.expand_or_jump()
-		elseif has_words_before() then
-			cmp.complete()
-		else
-			fallback()
-		end
-	end, { "i", "s" }),
-
-	["<S-Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_prev_item()
-		elseif luasnip.jumpable(-1) then
-			luasnip.jump(-1)
-		else
-			fallback()
-		end
-	end, { "i", "s" }),
+	["<Tab>"] = vim.NIL,
+	["<S-Tab>"] = vim.NIL,
+	["<CR>"] = vim.NIL,
 })
-cmp_mappings['<CR>'] = nil
 
 cmp.setup({
 	window = {
