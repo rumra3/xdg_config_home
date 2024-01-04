ba640c70d220ca39774ab8947d9a7a426c2ec043
ramura <44088736+rumra3@users.noreply.github.com>
2024-01-04 23:33:02 +0100

    proper linebreaks

--- nvim/lua/ramura/init.lua
+++ nvim/lua/ramura/init.lua
@@ -9,9 +9,12 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.smartindent = true
+vim.opt.smartindent = false
+vim.opt.cindent = true
 
 vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
